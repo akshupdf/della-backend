707ebc30d96ad4209e26f15751827de0193d745d
akshupdf <pednekar.akshay9@gmail.com>
2024-12-23 13:14:32 +0530

    try vercel v4

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const leadRoutes = require('./routes/leadRoutes');
-const connectDatabase = require('./db/db');
-const dotenv = require("dotenv")
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const leadRoutes = require("./routes/leadRoutes");
+const connectDatabase = require("./db/db");
+const dotenv = require("dotenv");
 
-//config
-dotenv.config({path:"db/config.env"})
+// Config
+dotenv.config({ path: "db/config.env" });
 
 // Initialize the app
 const app = express();
@@ -17,15 +17,12 @@ connectDatabase();
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/api/v1', leadRoutes);
-
+app.use("/api/v1", leadRoutes);
 
 // Root Route
-app.get('/', (req, res) => {
-  res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
-// Start Server
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on http://localhost:${process.env.PORT}`);
-});
+// Export the app for Vercel
+module.exports = app;
